4eb86fdcb3066e19965ae760239792f2fd753c1c
Leandro Rezende <leandro@MacBook-Pro-de-Leandro.local>
2021-11-13 09:49:48 -0300

    add zsh install

--- brew-install.sh
+++ brew-install.sh
@@ -2,4 +2,5 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-bash scrips/brew/visual-studio-code-install.sh
+bash scripts/brew/visual-studio-code-install.sh
+bash scripts/brew/zsh-install.sh
