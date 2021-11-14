ce03b7273b5341b292f3e5e6461abcbd78d5b152
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-11-14 10:00:57 -0300

    add tmux setup

---
+++ scripts/setup/tmux-setup.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+git clone https://github.com/thewtex/tmux-mem-cpu-load
+
+cd tmux-mem-cpu-load
+cmake .
+make
+sudo make install
+cd ..
+rm -rf tmux-mem-cpu-load
+
+cp scripts/dotfiles/.tmux.conf ~/
--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 
 bash scripts/setup/git-setup.sh
 bash scripts/setup/oh-my-zsh-setup.sh
+bash scripts/setup/tmux-setup.sh
 bash scripts/setup/zsh-setup.sh
