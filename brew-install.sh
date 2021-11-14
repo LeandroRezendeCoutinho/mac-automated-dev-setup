cc38c3e391357d7b0736de8f68bffdab964d2794
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-11-14 11:38:02 -0300

    sort alphabetically

--- brew-install.sh
+++ brew-install.sh
@@ -2,26 +2,26 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-bash scripts/brew/visual-studio-code-install.sh
-bash scripts/brew/zsh-install.sh
-bash scripts/brew/dbeaver-install.sh
-bash scripts/brew/docker-install.sh
-bash scripts/brew/free-download-manager.sh
-bash scripts/brew/sublime-text-install.sh
-bash scripts/brew/tig-install.sh
-bash scripts/brew/telegram-install.sh
-bash scripts/brew/xclip-install.sh
-bash scripts/brew/htop-install.sh
-bash scripts/brew/tmux-install.sh
-bash scripts/brew/iterm2-install.sh
-bash scripts/brew/google-chrome-install.sh
-bash scripts/brew/franz-install.sh
-bash scripts/brew/postman-install.sh
-bash scripts/brew/slack-install.sh
-bash scripts/brew/spotify-install.sh
-bash scripts/brew/zoom-install.sh
+bash scripts/brew/alt-tab-install.sh
 bash scripts/brew/asdf-install.sh
 bash scripts/brew/cmake-install.sh
-bash scripts/brew/alt-tab-install.sh
+bash scripts/brew/dbeaver-install.sh
+bash scripts/brew/docker-install.sh
+bash scripts/brew/franz-install.sh
+bash scripts/brew/free-download-manager.sh
+bash scripts/brew/google-chrome-install.sh
+bash scripts/brew/htop-install.sh
+bash scripts/brew/iterm2-install.sh
+bash scripts/brew/postman-install.sh
 bash scripts/brew/rectangle-install.sh
+bash scripts/brew/slack-install.sh
+bash scripts/brew/spotify-install.sh
 bash scripts/brew/stats-install.sh
+bash scripts/brew/sublime-text-install.sh
+bash scripts/brew/telegram-install.sh
+bash scripts/brew/tig-install.sh
+bash scripts/brew/tmux-install.sh
+bash scripts/brew/visual-studio-code-install.sh
+bash scripts/brew/xclip-install.sh
+bash scripts/brew/zoom-install.sh
+bash scripts/brew/zsh-install.sh
